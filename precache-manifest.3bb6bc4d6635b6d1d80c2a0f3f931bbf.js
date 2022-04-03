959b45d87d17bb6c13b2526976b8e9635c7f0d5f
escaper423 <escaper423@gmail.com>
2022-04-03 19:39:05 +0900

    Updates

---
+++ precache-manifest.3bb6bc4d6635b6d1d80c2a0f3f931bbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f8d563638bd4d306373",
+    "url": "/react-fe-test/static/css/main.1ffda62d.chunk.css"
+  },
+  {
+    "revision": "2f8d563638bd4d306373",
+    "url": "/react-fe-test/static/js/main.2f8d5636.chunk.js"
+  },
+  {
+    "revision": "5933624ef5c4c436412d",
+    "url": "/react-fe-test/static/js/1.5933624e.chunk.js"
+  },
+  {
+    "revision": "176417599c2d291bcd62",
+    "url": "/react-fe-test/static/js/2.17641759.chunk.js"
+  },
+  {
+    "revision": "b3999c1f77ffc3bc9385",
+    "url": "/react-fe-test/static/js/runtime~main.b3999c1f.js"
+  },
+  {
+    "revision": "3cc9ef2ab7f5844a840eee3576a9c82f",
+    "url": "/react-fe-test/index.html"
+  }
+];
